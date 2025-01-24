4df1566167942255cfef1a75ae8010f4153df47a
ivanayael <ivanyael@hotmail.com>
2025-01-23 23:56:21 -0300

    Adding exercises

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        myValue = "Y";
+        myValue2 = "y";
+
+        if myValue1.Trim().ToLower() != myValue2.Trim().ToLower()
+
+        console.WriteLine("El Resultado es Falso");
+
+        else console.WriteLine("El Resultado es Vendadero");
+
+
+
+    }
+}
